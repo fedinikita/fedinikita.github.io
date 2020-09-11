78c8fe1f675825e4d6b81ea2eaafba04c2046b20
fedinikita <57401736+fedinikita@users.noreply.github.com>
2020-09-12 00:20:44 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let arr = ['|  |  |', '/  /  /', '-  -  -', '\\  \\  \\'];
-let count = 0;
-
-setInterval(function () {
-    document.querySelector('.prek').innerHTML = arr[count % 4];
-    count++;
-}, 500);
